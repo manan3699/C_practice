e2d3d10af1e1b8d17bf7ff8cfdfdd08188372031
manan3699 <165313623+manan3699@users.noreply.github.com>
2026-03-21 17:59:05 +0530

    Add files via upload

---
+++ assi.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    float a=1.40,b=2.560;
+
+    printf("%f+%f=%f\n",a,b,a+b);
+    printf("%f-%f=%f\n",a,b,a-b);
+    printf("%f*%f=%f\n",a,b,a*b);
+    printf("%f/%f=%f\n",a,b,a/b);
+
+    return 0;
+}
