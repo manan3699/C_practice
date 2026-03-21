311f8e2b74ad36c3fb43dce0dd2b6bbe6a6d4204
manan3699 <165313623+manan3699@users.noreply.github.com>
2026-03-21 17:58:10 +0530

    Add files via upload

---
+++ New.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int A=20 , B=12,SUM;
+    SUM = A * B;
+    printf("%d * %d =%d", A, B, SUM);
+
+    // char name1="Man" , name2="an",Fullname;
+    // Fullname = name1 + name2;
+    // printf("My name is %c + %sc = %c",name1,name2,Fullname);
+    return 0;
+}
+
