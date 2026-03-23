8a5a6f0d6e5fb64ee4bc89b8a9a6fea816235b69
manan3699 <165313623+manan3699@users.noreply.github.com>
2026-03-23 15:02:21 +0530

    Add files via upload

---
+++ Day2.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int num1 = 10;
+    char ch = 'a';
+    float fvar = 2.1;
+    double dvar =3.65;
+
+    printf("The size of int =\n%d\n%d\n%d\n%d\n",sizeof(int),sizeof(ch),sizeof(float),sizeof(dvar)); 
+    
+
+    printf("manan patel\rjayaesh\n");
+    return 0;
+}
