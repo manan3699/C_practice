c707534c5e727b68058638ae15e33ced2cde7288
manan3699 <165313623+manan3699@users.noreply.github.com>
2026-03-23 15:02:39 +0530

    Add files via upload

---
+++ assi_2.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    char a;
+    printf("entre the value:");
+    scanf("%c",&a);
+    printf("the value is %d\n",a);
+    printf("the value is %lf\n",a);
+    printf("the value is %f\n",a);
+    printf("the value is %o\n",a);
+    printf("the value is %x\n",a);
+    return 0;
+}
