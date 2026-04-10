f488bc340bf45dd26096cfa7f9e12cdd5da3da66
manan3699 <165313623+manan3699@users.noreply.github.com>
2026-04-10 11:42:35 +0530

    Add files via upload

---
+++ day4.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+int main(){
+    // char ch=65;
+    // int x=9;
+    // printf("%d,%d,",sizeof(ch),sizeof('A'));
+    // printf("%d,%d,",sizeof(65),sizeof(x++));
+    // printf("%d",x);
+    // return 0;
+
+    // printf("%d", '\n' - '\r');
+    // printf("ASCII value of \\n is %d\n",'\n');
+    // printf("ASCII value of \\r is %d\n",'\r');
+    // printf("ASCII value of \\t is %d\n",'\t');
+    // printf("ASCII value of \\b is %d\n",'\b');
+    // printf("ASCII value of \\\" is %d\n",'\"');
+    // printf("ASCII value of \\a is %d\n",'\a');
+    // return 0;
+
+    // int x=300;
+    // printf("%u",x*x/x);
+    // return 0;
+
+    // int a= 3, b=6;
+    // printf("%d, ",a=b);
+    // printf("%d, ",a==b);
+    // printf("%d, ",a !=b);
+    // printf("%d, ",a=!b);
+
+    int x= -1;
+    printf("%u,%x ,%d", x>>1 , x<<4 ,(unsigned)x>>1);
+
+    return 0;
+
+}
